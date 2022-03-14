94630533c410b5c1b75816245ddebb22a3e7bae9
Harton <hartontw@gmail.com>
2022-03-14 17:47:43 +0100

    🗄️ Added Lib

---
+++ Lib/Vector.cs
@@ -0,0 +1,15 @@
+namespace rest_tilemap_3d_cs;
+
+public struct Vector
+{
+    public int x;
+    public int y;
+    public int z;
+
+    public Vector(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }    
+}
